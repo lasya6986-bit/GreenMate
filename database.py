583ac7e12d6b9c256c0e562528301388014cb159
lasya6986-bit <lasya6986@gmail.com>
2026-06-01 15:40:34 +0530

    Finished GreenMate version1 with CRUD operations and watering history

--- database.py
+++ database.py
@@ -5,5 +5,11 @@ cursor.execute("""
                create table if not exists plants(id integer primary key autoincrement,
                plant_name text not null,last_watered text,sunlight text)""")
 conn.commit()
+
+cursor.execute("""
+               Create table if not exists watering_history(history_id INTEGER PRIMARY KEY AUTOINCREMENT,
+               plant_id INTEGER, watering_date TEXT,FOREIGN KEY(plant_id) references plants(id))""")
+conn.commit()
+
 print("DataBase and table created successfully!")
 conn.close()
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ while(True):
     print("3. Update Watering Date")
     print("4. Delete plant")
     print("5. Search Plant")
-    print("6. Exit")
+    print("6. View Watering History")
+    print("7. Exit")
     choice=int(input("Enter your choice :"))
     if(choice==1):
         plant=input("Enter plant name : ")
@@ -16,6 +17,12 @@ while(True):
         sunlight=input("Enter sunlight requirement: ")
         cursor.execute("""
                        insert into plants (plant_name,last_watered,sunlight) values (?,?,?)""",(plant,watered,sunlight))
+        conn.commit()
+        plant_id=cursor.lastrowid
+       
+        cursor.execute(""" 
+                       insert
+                        into watering_history(plant_id,watering_date) values (?,?)""",(plant_id,watered))
         conn.commit()
         print("Plant added Successfully")
 
@@ -37,12 +44,16 @@ while(True):
         plant_id=int(input("Enter Plant ID : "))
         cursor.execute("""
                        select * from plants where id=?""",(plant_id,))
+        
         plant=cursor.fetchone()
         if(plant is not None):
             new_date=input("Enter new watered date : ")
             cursor.execute("""
                        update plants set last_watered=? where id=?
                        """,(new_date,plant_id))
+            conn.commit()
+            cursor.execute("""
+                           insert into watering_history(plant_id,watering_date) values(?,?)""",(plant_id,new_date))
             conn.commit()
             print("Watering date updated successfully!")
        
@@ -51,6 +62,8 @@ while(True):
             print("Plant ID NOT Found")
     elif choice==4:
         plant_id=int(input("Enter plant ID to be deleted : "))
+        cursor.execute("""
+                       delete from watering_history where plant_id=?""",(plant_id,))
         cursor.execute("""
                        delete from plants where id=?""",(plant_id,))
         if(cursor.rowcount==0):
@@ -61,7 +74,7 @@ while(True):
     elif choice==5:
         plant=input("Enter plant name :")
         cursor.execute("""
-                       select * from plants where plant_name= ?""",(plant,))
+                       select * from plants where lower(plant_name)= lower(?)""",(plant,))
         row=cursor.fetchone()
         if(row is None):
             print(plant," does not exists !")
@@ -73,9 +86,27 @@ while(True):
             print("Last_water_date : ",row[2])
             print("Level of Sunlight Required : ",row[3])   
 
-   
-    
     elif choice==6:
+        cursor.execute("select * from watering_history")
+        
+        rows=cursor.fetchall()
+        if len(rows)==0:
+            print("No plants added yet!")
+        else:
+            print("------------")
+            for row in rows:
+                cursor.execute("""
+                               select * from plants where id=?""",(row[1],))
+                row1=cursor.fetchone()
+                print(f"History ID: {row[0]}")
+                print(f"Plant Id : {row[1]}")
+                print(f"Plant Name : {row1[1]}")
+                print(f"Watering date : {row[2]}")
+                
+                print("------------")  
+
+    
+    elif choice==7:
         print("Thank you for using GreenMate!")
         break
     else:
